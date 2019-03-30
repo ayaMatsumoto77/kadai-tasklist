518113abb5071c1bfb47d9c3d7a4c71be059b23e
Matsumoto <ec2-user@ip-172-31-69-44.ec2.internal>
2019-03-30 09:21:33 +0000

    user registration

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,7 @@ Route::get("/", function () {
    return view("welcome"); 
 });
 Route::resource("tasks", "TasksController");
+
+// ユーザ登録
+Route::get("signup", "Auth\RegisterController@showRegistrationForm")->name("signup.get");
+Route::post("signup", "Auth\RegisterController@register")->name("signup.post");
